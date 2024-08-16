f7c83a79267b1785bcc8077cd7f488a83484b1ab
Manish Chavan <manish.chavan113@gmail.com>
2024-08-16 14:47:56 +0530

    fix boilerplate links

--- application/views/register-form.php
+++ application/views/register-form.php
@@ -1,23 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title></title>
+  <title><?php echo $page_title; ?></title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
   <!-- Favicons -->
-  <link href="<?php echo base_url(); ?>assets/admin/assets/img/favicon.png" rel="icon">
-  <link href="<?php echo base_url(); ?>assets/admin/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="<?php echo base_url(); ?>assets/img/favicon.png" rel="icon">
+  <link href="<?php echo base_url(); ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA=" crossorigin="anonymous" />
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" integrity="sha512-nMNlpuaDPrqlEls3IX/Q56H36qvBASwb3ipuo3MxeWbsQB1881ox0cRv7UPTgBlriqoynt35KjEwgGUeUXIPnw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  
   <!-- Vendor CSS Files -->
-  <link href="<?php echo base_url(); ?>assets/admin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?php echo base_url(); ?>assets/admin/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
-  <link href="<?php echo base_url(); ?>assets/admin/assets/css/style.css" rel="stylesheet">
+  <!-- Main CSS File -->
+  <link href="<?php echo base_url(); ?>assets/css/main.css" rel="stylesheet">
   <link href="<?php echo base_url(); ?>assets/css/custom.css" rel="stylesheet">
 </head>
 <body>
@@ -28,12 +42,6 @@
           <div class="row justify-content-center">
             <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
-            <div class="d-flex justify-content-center py-4">
-                <a href="<?php echo base_url(); ?>home" class="logo d-flex align-items-center w-auto">
-                   <img src="<?php echo base_url(); ?>assets/admin/assets/img/logo.png" alt="">
-                </a>
-              </div><!-- End Logo -->
-
               <div class="col">
                 <?php
                   if($this->session->flashdata('status')) : ?>
@@ -43,7 +51,7 @@
                 <?php endif; ?>
               </div>
               
-              <div class="card mb-3 p-3">
+              <div class="card mb-3 p-3 border-0 shadow">
 
                 <div class="card-body">
 
@@ -109,12 +117,14 @@
   <a href="<?php echo base_url(); ?>#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 <!-- Vendor JS Files -->
-<script src="<?php echo base_url(); ?>assets/admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/admin/assets/vendor/tinymce/tinymce.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/admin/assets/vendor/php-email-form/validate.js"></script>
+<script src="<?php echo base_url(); ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/vendor/php-email-form/validate.js"></script>
+
 
 <!-- Template Main JS File -->
-<script src="<?php echo base_url(); ?>assets/admin/assets/js/main.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/main.js"></script>
+
+
 
 </body>
 </html>
